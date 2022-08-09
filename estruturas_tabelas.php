867c89870cb887af2edaed02367c14936613717e
GuiEnrik <guienrik@gmail.com>
2022-08-09 12:26:15 -0300

    Create estruturas_tabelas.php

---
+++ estruturas_tabelas.php
@@ -0,0 +1,49 @@
+<?php
+
+$mysql_host = "sql102.epizy.com";
+$mysql_user = "epiz_31738243";
+$mysql_password = "hLyqBJscqLmH";
+$mysql_database = "epiz_31738243_meubanco2";
+
+if (!mysql_connect($mysql_host, $mysql_user, $mysql_password)) {
+    echo "Não foi possível conectar ao mysql";
+    exit;
+}
+
+echo "<pre>";
+
+$sql = "SHOW TABLES FROM $mysql_database";
+$result = mysql_query($sql);
+
+if (!$result) {
+    echo "Erro, não foi possivel listar as tabelas do banco\n";
+    echo "Erro MySQL: " . mysql_error();
+    exit;
+}
+
+while ($row = mysql_fetch_row($result)) {
+    echo "TABELA: {$row[0]}\n";
+
+    $sql2 = "DESCRIBE ". $row[0];
+
+    mysql_query("USE ".$mysql_database);
+
+    $result2 = mysql_query($sql2);
+
+    if (!$result2) {
+        echo "Erro, não foi possivel listar as colunas da tabela\n";
+        echo "Erro MySQL: " . mysql_error();
+        exit;
+    }
+    while($row2 = mysql_fetch_array($result2)) {
+        echo "{$row2['Field']} - {$row2['Type']}\n";
+    }
+
+    echo "\n";
+
+}
+
+echo "</pre>";
+
+mysql_free_result($result);
+?>
